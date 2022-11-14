f507a01d23d9b4a9a0f7a4b9c7322758982b4c83
Ali-Adel-Nour <aliiiadel841@gmail.com>
2022-11-14 10:36:25 +0200

    new

--- e-commerce/resources/views/layouts/admin.blade.php
+++ e-commerce/resources/views/layouts/admin.blade.php
@@ -40,18 +40,18 @@
 
 
     <!-- plugins:js -->
-  <script src="{{asset('vendors/base/vendor.bundle.base.js')}}"></script>
+  <script src="{{asset('admin/vendors/base/vendor.bundle.base.js')}}"></script>
 
-  <script src="{{asset('vendors/datatables.net/jquery.dataTables.js')}}"></script>
-  <script src="{{asset('vendors/datatables.net-bs4/dataTables.bootstrap4.js')}}"></script>
-  <script src="{{asset('js/off-canvas.js')}}"></script>
-  <script src="{{asset('js/hoverable-collapse.js')}}"></script>
-  <script src="{{asset('js/template.js')}}"></script>
+  <script src="{{asset('admin/vendors/datatables.net/jquery.dataTables.js')}}"></script>
+  <script src="{{asset('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js')}}"></script>
+  <script src="{{asset('admin/js/off-canvas.js')}}"></script>
+  <script src="{{asset('admin/js/hoverable-collapse.js')}}"></script>
+  <script src="{{asset('admin/js/template.js')}}"></script>
   <!-- Custom js for this page-->
-  <script src="{{asset('js/dashboard.js')}}"></script>
-  <script src="{{asset('js/data-table.js')}}"></script>
-  <script src="{{asset('js/jquery.dataTables.js')}}"></script>
-  <script src="{{asset('js/dataTables.bootstrap4.js')}}"></script>
+  <script src="{{asset('admin/js/dashboard.js')}}"></script>
+  <script src="{{asset('admin/js/data-table.js')}}"></script>
+  <script src="{{asset('admin/js/jquery.dataTables.js')}}"></script>
+  <script src="{{asset('admin/js/dataTables.bootstrap4.js')}}"></script>
   <!-- End custom js for this page-->
     @livewireScripts
 </body>
--- e-commerce/routes/web.php
+++ e-commerce/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +22,15 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::prefix('admin')->group(function(){
+Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function(){
     Route::get('dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
 
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
